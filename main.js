91aab8cbcaaf7de940c4e5e5cbcdbbd78cc3aa50
Pablo Bosak <pablopvb@gmail.com>
2021-10-26 17:35:34 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const LinksSocialMedia = {
+  github: 'pablo-bosak',
+  youtube: 'S33zuRtPcOYgKej2NmSU8g',
+  linkedin: 'in/pablo-bosak',
+  instagram: 'pablo_bosak',
+  twitter: 'pablo_bosak'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      //userName.textContent = data.name
+      //userBio.textContent = data.bio
+      //userLink.href = data.html_url
+      //UserImage.src = data.avatar_url
+      //userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
